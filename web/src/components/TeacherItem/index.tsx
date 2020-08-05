7b52a087236c245455e1e14d6bf7c5fa1299df6a
Gabriel Luciano <gomeslucianogabriel@gmail.com>
2020-08-05 12:18:04 -0300

    TeacherItem Component + Teacher Interface

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -5,31 +5,38 @@ import whatsAppIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-const TeacherItem = () => {
+export interface Teacher {
+  id: number,
+  avatar: string,
+  bio: string,
+  cost: number,
+  name: string,
+  subject: string,
+  whatsapp: string,
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+  
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://pbs.twimg.com/profile_images/1119592201791655938/JVsVIApm_400x400.jpg" alt="Gabriel Luciano" />
+        <img src={teacher.avatar} alt="Gabriel Luciano" />
         <div>
-          <strong> Gabriel Luciano</strong>
-          <span>Redes de Computadores</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Recém formado em Ciência da Computação pela PUCMINAS e mestrando em Ciência da Computação na UNICAMP,
-        tenho um breve conhecimento em Redes de Computadores e gostaria de compartilhar o máximo de conhecimento
-        possívels para aqueles que tiver o interesse.
-
-      <br />
-        <br />
-      Redes de Computadores não é difícil, só te prega peças que rendem alguns fios de cabelos.
-    </p>
+      <p> {teacher.bio} </p>
 
       <footer>
         <p>
           Preço/hora
-        <strong>R$ 20,00</strong>
+        <strong>{`R$ ${teacher.cost}`}</strong>
         </p>
         <button type="button">
           <img src={whatsAppIcon} alt="WhatsApp" />
